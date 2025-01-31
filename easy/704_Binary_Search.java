a3c0d234f2921b86b274e47d113eed2b67b9c017
SHIAK AFRID <afridayan01@gmail.com>
2025-01-31 11:01:35 +0530

    Create 704_Binary_Search.java

---
+++ easy/704_Binary_Search.java
@@ -0,0 +1,11 @@
+class Solution {
+    public static int search(int[] nums, int target) {
+        for(int index = 0;index<nums.length;index++){
+            if(nums[index] > target){
+                return -1;
+            }
+            if(nums[index] == target) return index;
+        }
+        return -1;
+    }
+}
